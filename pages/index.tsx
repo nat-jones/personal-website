850319e12ba046910f3d0aaa268f5eca3129a8c1
Nathaniel Jones <natjoe4@Nathaniel-Joness-MacBook-Pro-2.local>
2023-09-19 13:11:18 -0400

    preload background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,20 @@ import { useState } from 'react';
 import Menu from '../components/Menu';
 import PageSelector from '../components/PageSelector';
 import { AllPagesType } from '../types';
+import Head from 'next/head';
 
 export default function HomePage() {
   const [pageInfo, setPageInfo] = useState<AllPagesType>('homepage');
   return (
-    <BackgroundImage src="./images/surfing.jpeg">
-      <Menu page={pageInfo} setPageInfo={setPageInfo}>
-        <PageSelector pageInfo={pageInfo} />
-      </Menu>
-    </BackgroundImage>
+    <>
+      <Head>
+        <link rel="preload" href="./images/surfing.jpeg" as="image" />
+      </Head>
+      <BackgroundImage src="./images/surfing.jpeg">
+        <Menu page={pageInfo} setPageInfo={setPageInfo}>
+          <PageSelector pageInfo={pageInfo} />
+        </Menu>
+      </BackgroundImage>
+    </>
   );
 }
